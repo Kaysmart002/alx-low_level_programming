5cf170449601795e3c51a731001dfec0cd963e44
Kaysmart <lkayode66@gmail.com>
2023-07-14 16:10:18 -0700

    task 5 done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+  * main - this is where the program starts
+  * Return: 0
+  */
+int main(void)
+{
+	int n;
+for (n = 0; n < 10; n++)
+{
+	printf("%d", n);
+}
+printf("\n");
+return (0);
+}
