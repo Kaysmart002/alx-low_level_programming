9ec75f23b92cec248f8dfec5c30fd14dccf44e6d
Kaysmart <lkayode66@gmail.com>
2023-07-14 20:53:06 -0700

    task 6 done

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+  * main - the program starts here
+  * Return: Always 0 on success
+  */
+int main(void)
+{
+	int num;
+for (num = 48; num < 58; num++)
+{
+	putchar(num);
+}
+putchar('\n');
+return (0);
+}
