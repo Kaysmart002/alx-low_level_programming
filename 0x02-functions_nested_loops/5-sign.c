0494c40c67a18029bc71059059da09ff4893f4d0
Kaysmart <lkayode66@gmail.com>
2023-07-18 16:50:05 -0700

    Hard

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+  * print_sign - Entry point
+  * @n: what to be checked
+  * Return: 1 on success
+  */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		return (0);
+		_putchar('0');
+	}
+	else
+	{
+		return (-1);
+		_putchar('-');
+	}
+}
---
+++ 0x02-functions_nested_loops/sign.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+int main()
+{
+	int r = print_sign('4');
+	_putchar(r);
+	return 0;
+}
